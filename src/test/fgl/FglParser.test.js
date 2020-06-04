00e63bbabfa04eba732c28514bb0c9e79c32ae7d
anbern <banzeletti@gmx.at>
2020-06-04 15:04:48 +0200

    TestCases polished, matchTree(...)

--- src/test/fgl/FglParser.test.js
+++ src/test/fgl/FglParser.test.js
@@ -1,14 +1,10 @@
 import { ScanNoWhitespace } from '../../main/fgl/FglScanner';
-//import { FunctionInvocationNode, NumberLiteralNode } from '../../main/fgl/FglAst';
 import { Parse } from '../../main/fgl/FglParser';
 
 test('simple number literal',() => {
     const tokens = ScanNoWhitespace({ source: '1'} );
     expect(tokens).toHaveLength(2);
-    //console.log(tokens);
     const ast = Parse(tokens);
-    //console.log('simple number literal test: ast=');
-    //printAstNode(0,ast);
     expect(ast).toMatchObject({
        token: {
            lexxem: '1'
@@ -20,104 +16,146 @@ test('simple number literal',() => {
 test('simple multiplication',() => {
     const tokens = ScanNoWhitespace({ source: '2 * 3'} );
     expect(tokens).toHaveLength(4); //including EOF
-    //console.log(tokens);
     const ast = Parse(tokens);
-    //console.log('simple multiplication test: ast=');
-    //printAstNode(0,ast);
-    expect(ast).toMatchObject({
-       identifier: '*',
-    });
-    expect(ast.children).toBeDefined();
-    expect(ast.children[0]).toMatchObject({token: { lexxem: '2'}});
-    expect(ast.children[1]).toMatchObject({token: { lexxem: '3'}});
+    const expected = {
+        identifier: '*',
+        leftSide: {
+            number: '2'
+        },
+        rightSide: {
+            number: '3'
+        }
+    };
+    matchTree(ast,expected);
 })
 
 test('combined multiplication / division ',() => {
     const tokens = ScanNoWhitespace({ source: '2 * 3 / 4'} );
     expect(tokens).toHaveLength(6); //including EOF
-    //console.log(tokens);
     const ast = Parse(tokens);
-    //console.log('combined multiplication / division test: ast=');
-    //printAstNode(0,ast);
-    expect(ast).toMatchObject({identifier: '*'});
-    expect(ast.children).toBeDefined();
-    expect(ast.children).toHaveLength(2);
-    const l0left  = ast.children[0];
-    const l0right = ast.children[1];
-
-    expect(l0left).toMatchObject({token:{lexxem:'2'}});
-    expect(l0right).toMatchObject({identifier:'/'});
-    expect(l0right.children).toBeDefined();
-    expect(l0right.children).toHaveLength(2);
-    const l1left  = l0right.children[0];
-    const l1right = l0right.children[1];
-
-    expect(l1left).toMatchObject({token:{lexxem:'3'}});
-    expect(l1right).toMatchObject({token:{lexxem:'4'}});
+    const expected = {
+        identifier: '*',
+        leftSide: {
+            number: '2'
+        },
+        rightSide: {
+            identifier: '/',
+            leftSide: {
+                number: '3'
+            },
+            rightSide: {
+                number: '4'
+            }
+        }
+    };
+    matchTree(ast,expected);
 
 })
 
 test('combined addition and multiplication (* rechts)',() => {
     const tokens = ScanNoWhitespace({ source: '2 + 3 * 4'} );
     expect(tokens.length).toBe(6); //including EOF
-    //console.log(tokens);
     const ast = Parse(tokens);
-    //console.log('combined addition and multiplication (* rechts) test: ast=');
-    //printAstNode(0,ast);
-    expect(ast).toMatchObject({identifier: '+'});
-    expect(ast.children).toHaveLength(2);
-    const l0left = ast.children[0];
-    const l0right = ast.children[1];
-    expect(l0left).toMatchObject({token:{lexxem:'2'}});
-    expect(l0right).toMatchObject({identifier: '*'});
-    expect(l0right.children).toHaveLength(2);
-    const l1left = l0right.children[0];
-    const l1right = l0right.children[1];
-    expect(l1left).toMatchObject({token:{lexxem:'3'}});
-    expect(l1right).toMatchObject({token:{lexxem:'4'}});
+    const expected = {
+        identifier: '+',
+        leftSide: {
+            number: '2'
+        },
+        rightSide: {
+            identifier: '*',
+            leftSide: {
+                number: '3'
+            },
+            rightSide: {
+                number: '4'
+            }
+        }
+    };
+    matchTree(ast,expected)
 })
 
 test('combined addition and multiplication (+ rechts)',() => {
     const tokens = ScanNoWhitespace({ source: '2 * 3 + 4'} );
     expect(tokens.length).toBe(6); //including EOF
-    //console.log(tokens);
     const ast = Parse(tokens);
-    //console.log('combined addition and multiplication (+ rechts) test: ast=');
-    //printAstNode(0,ast);
-    expect(ast).toMatchObject({identifier: '+'});
-    expect(ast.children).toHaveLength(2);
-    const l0left = ast.children[0];
-    const l0right = ast.children[1];
-    expect(l0left).toMatchObject({identifier:'*'});
-    expect(l0left.children).toHaveLength(2);
-    const l1left = l0left.children[0];
-    const l1right = l0left.children[1];
-    expect(l1left).toMatchObject({token:{lexxem:'2'}});
-    expect(l1right).toMatchObject({token:{lexxem:'3'}});
-    expect(l0right).toMatchObject({token:{lexxem:'4'}});
+    const expected = {
+      identifier: '+',
+      leftSide: {
+          identifier: '*',
+          leftSide: {
+              number: '2'
+          },
+          rightSide: {
+              number: '3'
+          }
+      },
+      rightSide: {
+          number: '4'
+      }
+    };
+    matchTree(ast,expected);
 })
 
 test('complex infix',() => {
     const tokens = ScanNoWhitespace({ source: '2 * 3 > 5 & 4 - 1 ^= 2'} );
     expect(tokens.length).toBe(12); //including EOF
-    //console.log(tokens);
     const ast = Parse(tokens);
-    //console.log('complex infix 2*3 > 5 & 4-1 ^= 2 test: ast=');
-    //printAstNode(0,ast);
-    console.log(ast);
+    const expected = {
+        identifier: '&',
+        leftSide: {
+            identifier: '>',
+            leftSide: {
+                identifier: '*',
+                leftSide: {
+                    number: '2'
+                },
+                rightSide: {
+                    number: '3'
+                }
+            },
+            rightSide: {
+                number: '5'
+            }
+        },
+        rightSide: {
+            identifier: '^=',
+            leftSide: {
+                identifier: '-',
+                leftSide: {
+                    number: '4'
+                },
+                rightSide: {
+                    number: '1'
+                }
+            },
+            rightSide: {
+                number: '2'
+            }
+        }
+    }
+    matchTree(ast,expected);
+
 })
+
 /*
  * Helper Functions
  */
-/*
-function printAstNode(level, astNode) {
-    if (astNode instanceof NumberLiteralNode) {
-        console.log(level + ' Number ' + astNode.token.lexxem);
-    } else if (astNode instanceof FunctionInvocationNode) {
-        console.log(level + ' Invoking ' + astNode.identifier + ' on ');
-        astNode.children.forEach(child => printAstNode(level + 1, child));
-    } else {
-        throw new Error("Miracolous astNode encountered " + astNode);
+
+function matchTree(ast,expected) {
+    if (expected.identifier) {
+        matchBinOp(ast,expected);
+    } else if (expected.number) {
+        matchNumberLiteral(ast,expected);
     }
 }
- */
+function matchBinOp(ast,operator) {
+    expect(ast).toMatchObject({identifier:operator.identifier});
+    expect(ast.children).toHaveLength(2);
+    const leftSide = ast.children[0];
+    const rightSide = ast.children[1];
+    matchTree(leftSide,operator.leftSide);
+    matchTree(rightSide,operator.rightSide);
+}
+function matchNumberLiteral(ast,numberLiteral) {
+    expect(ast).toMatchObject({token:{lexxem:numberLiteral.number}});
+}
